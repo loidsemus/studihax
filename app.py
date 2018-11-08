642afb4aaa25f641527de4069325234e223bf560
Emil Carlén <emil.j.carlen@gmail.com>
2018-11-08 17:57:03 +0100

    Add working auth

--- app.py
+++ app.py
@@ -1,33 +1,34 @@
-import requests
-from time import sleep
-import json
+from json import loads
 
-while True:
-    id = input("ID: ")
-    if not id.isdigit():
-        print("ID måste vara en siffra")
-        continue
-    break
+from requests import get, post
+
+from creds import authorize
+
+auth = authorize()
 
 headers = {
     'Content-Type': "application/json",
     'cache-control': "no-cache",
-    'authorization': "Bearer IfvQEKEIDQngklrMERD3TkanLQaYKUVtFaALlmTh",
+    'authorization': "Bearer %s" % auth['token'],
     'Postman-Token': "36cc45e2-6417-41fb-9bba-57695f89e215"
 }
 
-loginUrl = "https://api.studi.se/users/%s" % id
+detailsUrl = "https://api.studi.se/users/%s" % auth['user_id']
 
-loginResponse = requests.request("GET", loginUrl, headers=headers)
-loginData = json.loads(loginResponse.text)
-print('Inloggad som %s, du har %d poäng' % (loginData['display_name'], loginData['points']))
+detailsResponse = get(detailsUrl, headers=headers)
+detailsData = loads(detailsResponse.text)
+print('Inloggad som %s, du har %d poäng' % (detailsData['display_name'], detailsData['points']))
 
 url = "https://api.studi.se/lessons/quizReport"
-payload = "{\"quiz_level_id\":2278,\"user_id\":" + id + ",\"result\":[{\"question_id\":17362,\"result\":true,\"answers\":[\"0.7%\"],\"language_code\":\"sv\"},{\"question_id\":17361,\"result\":true,\"answers\":[\"0.2%\"],\"language_code\":\"sv\"},{\"question_id\":17364,\"result\":true,\"answers\":[\"4.6%\"],\"language_code\":\"sv\"},{\"question_id\":17363,\"result\":true,\"answers\":[\"0.0035\"],\"language_code\":\"sv\"},{\"question_id\":17360,\"result\":true,\"answers\":[\"0.075\"],\"language_code\":\"sv\"}],\"subject_id\":55686,\"passed\":true}"
+payload = "{\"quiz_level_id\":2278,\"user_id\":" + auth[
+    'user_id'] + ",\"result\":[{\"question_id\":17362,\"result\":true,\"answers\":[\"0.7%\"],\"language_code\":\"sv\"},{\"question_id\":17361,\"result\":true,\"answers\":[\"0.2%\"],\"language_code\":\"sv\"},{\"question_id\":17364,\"result\":true,\"answers\":[\"4.6%\"],\"language_code\":\"sv\"},{\"question_id\":17363,\"result\":true,\"answers\":[\"0.0035\"],\"language_code\":\"sv\"},{\"question_id\":17360,\"result\":true,\"answers\":[\"0.075\"],\"language_code\":\"sv\"}],\"subject_id\":55686,\"passed\":true}"
+
+
+def send_request():
+    response = post(url, data=payload, headers=headers)
+    data = loads(response.text)
+    print('Fick %s poäng' % data['points'])
 
 
 while True:
-    sleep(0.75)
-    response = requests.request("POST", url, data=payload, headers=headers)
-    data = json.loads(response.text)
-    print('Fick %s poäng' % data['points'])
+    send_request()
---
+++ creds.py
@@ -0,0 +1,32 @@
+import getpass
+from json import loads
+
+from requests import post
+
+url = 'https://api.studi.se/oauth/access_token'
+headers = {
+    'Content-Type': "application/json",
+    'cache-control': "no-cache",
+    'Postman-Token': "771e3906-dc7e-4d43-a948-82e23b9b5318"
+}
+
+
+def authorize():
+    while True:
+        username = input('Namn: ')
+        password = getpass.getpass('Lösenord: ')
+
+        payload = "{\"grant_type\":\"password\",\"username\":\"" + username + "\",\"password\":\"" + password + "\",\"client_id\":\"studi_ember\",\"client_secret\":\"ED1DA59DFCB83A43616CE714189CE\",\"scope\":\"view_lessons\"}\r\n"
+
+        response = post(url=url, data=payload, headers=headers)
+        data = loads(response.text)
+
+        if 'error_message' in data:
+            print('Fel lösenord eller namn')
+            continue
+        break
+
+    return {
+        'token': data['access_token'],
+        'user_id': str(data['user_id'])
+    }
